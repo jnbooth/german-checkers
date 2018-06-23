027c21365994bbb37148e7a7e16ced2582d53f3f
Joshua Booth <joshua.n.booth@gmail.com>
2018-06-23 01:09:54 -0700

    no message

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# german_checkers
-Fun fact: Chinese Checkers was invented in Germany and was eventually introduced to China by Japanese players.
+A simple game solver in Rust to make me less terrible at Chinese Checkers. The board state is represented by a 128-bit number where 0 is empty, 1 is full, and each binary digit corresponds to a location on the board:
+
+                    00
+                  02  01
+                05  04  03
+              09  08  07  06
+            14  13  12  11  10
+          20  19  18  17  16  15
+        27  26  25  24  23  22  21
+      35  34  33  32  31  30  29  28
+    44  43  42  41  40  39  38  37  36
+      52  51  50  49  48  47  46  45
+        59  58  57  56  55  54  53
+          65  64  63  62  61  60
+            70  69  68  67  66
+              74  73  72  71
+                77  76  75
+                  79  78
+                    80
---
+++ main.rs
@@ -0,0 +1,200 @@
+extern crate itertools;
+
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::iter::repeat;
+
+const GAME_SIZE: u8 = 1+2+3+4+5+6+7+8+9+8+7+6+5+4+3+2+1;
+
+fn to_bit(n: &u8) -> u128 {
+    2u128.pow(*n as u32)
+}
+fn test_bit(bits: &u128, n: &u8) -> bool {
+    bits & (1u128 << n) != 0
+}
+
+// using foldl (\xs x -> xs + 2 ^ x) 0
+// [0, 2, 5, 9, 14, 20, 27, 35, 44, 52, 59, 65, 70, 74, 77, 79, 80]
+const EDGE_LEFT:  u128 = 1984611988896094432018981;
+// [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 53, 60, 66, 71, 75, 78, 80]
+const EDGE_RIGHT: u128 = 1551372338562930514625611;
+
+#[derive(Debug, PartialEq)]
+enum Dir { FlatLeft
+         , FlatRight 
+         , UpLeft
+         , UpRight
+         , DownLeft
+         , DownRight
+         }
+
+#[derive(Debug)]
+struct Move { n:   u8
+            , dir: Dir
+            }
+
+impl Move {
+    pub fn new(n: i8, dir: Dir) -> Option<Move> { 
+        if n > 0 { Some(Move { n: n as u8, dir: dir }) }
+        else     { None }
+        
+    }
+}
+
+fn add_some<T>(xs: &mut Vec<T>, x: Option<T>) {
+    if let Some(x_) = x { xs.push(x_ )}
+}
+
+fn neighbors(n: &u8) -> Vec<Move> {
+    let mut xs    = Vec::new();
+    let     n_    = *n as i8;
+    let     row   = row_size(n);
+    let     left  = !test_bit(&EDGE_LEFT,  n);
+    let     right = !test_bit(&EDGE_RIGHT, n);
+    let     up    = n_ > 44;
+    let     up_   = !up as i8;
+    let     down  = n_ > 35;
+    let     down_ = !down as i8;
+
+    if left {
+        add_some(&mut xs, Move::new(n_ + 1, Dir::FlatLeft));
+    }
+    if right {
+        add_some(&mut xs, Move::new(n_ - 1, Dir::FlatRight));
+    }
+    if right || up {
+        add_some(&mut xs, Move::new(n_ - row + up_ - 1, Dir::UpRight));
+    }
+    if right || !up {
+        add_some(&mut xs, Move::new(n_ + row + down_ - 1, Dir::DownRight));
+    }
+    if left || up {
+        add_some(&mut xs, Move::new(n_ - row + up_, Dir::UpLeft));
+    }
+    if left || !up {
+        add_some(&mut xs, Move::new(n_ + row + down_, Dir::DownLeft));
+    }
+    xs.retain(|x| x.n < GAME_SIZE);
+    xs
+}
+
+fn slide(game: &u128, n: &u8) -> Vec<u128> {
+    let game_ = game & !to_bit(&n);
+    neighbors(n)
+        .into_iter()
+        .map(|x| x.n)
+        .filter(|n| !test_bit(game, &n))
+        .map(|n| game_ | to_bit(&n))
+        .collect()
+}
+
+fn hop(game: &u128, n: &u8) -> Vec<u8> {
+    neighbors(n)
+        .into_iter()
+        .filter(|n1| test_bit(&game, &n1.n))
+        .flat_map(|n1| neighbors(&n1.n)
+            .into_iter()
+            .filter(|n2| (n2.dir == n1.dir) && !test_bit(&game, &n2.n))
+            .map(|n2| n2.n)
+            .collect::<Vec<u8>>())
+        .collect()
+}
+
+fn add_hops(hopped: &mut HashSet<u128>, game: &u128, n: &u8) {
+    let game0 = game & !to_bit(n);
+    for n1 in hop(game, n) { 
+        let game1 = game0 | to_bit(&n1);
+        if hopped.insert(game1 | to_bit(&n1)) {
+            add_hops(hopped, &game1, &n1);
+        }
+    }
+}
+
+fn hops(game: &u128) -> HashSet<u128> {
+    let mut hopped = HashSet::new();
+    hopped.insert(*game);
+    for n in (0..GAME_SIZE).filter(|n| test_bit(game, n)) {
+        add_hops(&mut hopped, &game, &n);
+    }
+    hopped.remove(game);
+    hopped
+}
+
+fn play(games: &mut HashSet<u128>, game: &u128) -> Vec<u128> {
+    let mut moves = (0..GAME_SIZE)
+        .filter(|n| test_bit(game, n))
+        .flat_map(|n| slide(game, &n))
+        .collect::<Vec<u128>>();
+    moves.extend(hops(game));
+    moves.retain(|&x| games.insert(x));
+    moves
+}
+
+fn show_tiles(i: isize, xs: u128) -> String {
+    if i == 0 { String::new() } 
+    else {
+        let abs_i = i.abs();
+        let abs_z = abs_i as usize;
+        let pow   = 2u128.pow(abs_i as u32);
+        let mut s = format!("{:b}", xs % pow);
+        s         = repeat('0').take(abs_z - s.len()).collect::<String>() + &s;
+        format!( "{}{}\n{}"
+               , repeat(' ').take(9 - abs_z).collect::<String>()
+               , s.chars().intersperse(' ').fuse().collect::<String>()
+               , &show_tiles(if i == 8 {-9} else {i + 1}, xs / pow)
+               )
+    }
+}
+
+fn row_size(n: &u8) -> i8 {
+         if n == &0 { 1 }
+    else if n < &3  { 2 }
+    else if n < &6  { 3 }
+    else if n < &10 { 4 }
+    else if n < &15 { 5 }
+    else if n < &21 { 6 }
+    else if n < &28 { 7 }
+    else if n < &36 { 8 }
+    else if n < &45 { 9 }
+    else if n < &53 { 8 }
+    else if n < &60 { 7 }
+    else if n < &66 { 6 }
+    else if n < &71 { 5 }
+    else if n < &75 { 4 }
+    else if n < &78 { 3 }
+    else if n < &80 { 2 }
+    else            { 1 }
+}
+
+fn solve() -> Vec<u128> {
+    let     games   = &mut HashSet::new();
+    let     start   = (71..81).map(|x| to_bit(&x)).sum();
+    let     end     = (0 ..10).map(|x| to_bit(&x)).sum();
+    let mut playing = vec![vec![start]];
+    loop {
+        println!("{}: {}", playing[0].len(), playing.len());
+        let mut playing_: Vec<Vec<u128>> = Vec::new();
+        for turns in &playing {
+            if let Some(turn) = turns.last() {
+                let played = play(games, &turn);
+                if played.contains(&end) {
+                    return turns.clone();
+                }
+
+                playing_.extend(played
+                    .into_iter()
+                    .map(|game| {
+                        let mut turns_ = turns.clone();
+                        turns_.push(game);
+                        turns_
+                    }));
+            }
+        }
+        playing = playing_;
+    }
+}
+
+fn main() {
+    println!("{}", solve().into_iter().map(|x| show_tiles(1, x)).join("\n***\n"));
+    println!("***\n{}", show_tiles(1, (0 ..10).map(|x| to_bit(&x)).sum()));
+}
